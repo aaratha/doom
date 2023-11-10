4d907e172ee8ac602b5899ab8fd396816d2d5132
Aseem Ratha <aseemratha@gmail.com>
2023-11-10 15:49:16 -0500

    the end

--- config.el
+++ config.el
@@ -158,3 +158,9 @@
 (package-install 'treemacs-evil)
 
 (use-package treemacs)
+
+(defun transparent(alpha-level no-focus-alpha-level)
+ "Let's you make the window transparent"
+ (interactive "nAlpha level (0-100): \nnNo focus alpha level (0-100): ")
+ (set-frame-parameter (selected-frame) 'alpha (list alpha-level no-focus-alpha-level))
+ (add-to-list 'default-frame-alist `(alpha ,alpha-level)))
--- custom.el
+++ custom.el
@@ -3,15 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("3cdd0a96236a9db4e903c01cb45c0c111eb1492313a65790adb894f9f1a33b2d" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(org-agenda-files '("~/OneDrive/org/school.org"))
  '(package-selected-packages
-   '(tabify uimage spell-fu org-super-agenda org-modern org-fragtog olivetti magit latex-preview-pane fountain-mode ein dracula-theme auctex all-the-icons @)))
+   '(smooth-scrolling smooth-scroll treemacs-evil treemacs tabify uimage spell-fu org-super-agenda org-modern org-fragtog olivetti magit latex-preview-pane fountain-mode ein dracula-theme auctex all-the-icons @)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#2E3440" :foreground "#ECEFF4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "outline" :family "Consolas")))))
 (put 'customize-face 'disabled nil)
 (put 'customize-themes 'disabled nil)
 (put 'customize-variable 'disabled nil)
