dd8ade3d50fa04b013be6c06df84fe3ec1376718
Aseem Ratha <aseemratha@gmail.com>
2023-11-12 13:48:02 -0500

    added school to agenda on windows

--- custom.el
+++ custom.el
@@ -3,11 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("3cdd0a96236a9db4e903c01cb45c0c111eb1492313a65790adb894f9f1a33b2d" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
- '(org-agenda-files
-   '("~/Library/CloudStorage/OneDrive-Personal/org/school.org"))
+ '(org-agenda-files '("~/OneDrive/org/school.org"))
  '(package-selected-packages
-   '(doom auctex treemacs writeroom-mode treemacs-magit treemacs-evil treemacs-all-the-icons org-xlatex org-super-agenda org-modern org-bullets olivetti fountain-mode ein)))
+   '(treemacs writeroom-mode treemacs-magit treemacs-evil treemacs-all-the-icons org-xlatex org-super-agenda org-modern org-bullets olivetti fountain-mode ein)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
