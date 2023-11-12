4e264caf9824da083948f23a0cd71daf6008e8a3
Aseem Ratha <aseemratha@gmail.com>
2023-11-12 17:55:02 -0500

    nerd fonts...?

--- config.el
+++ config.el
@@ -166,7 +166,7 @@
  "Let's you make the window transparent"
  (interactive "nAlpha level (0-100): \nnNo focus alpha level (0-100): ")
  (set-frame-parameter (selected-frame) 'alpha (list alpha-level no-focus-alpha-level))
- (add-to-list 'default-frame-alist `(alpha ,alpha-level)))
+ (add-to-list 'default-frame-alist `(alpha ,alpha-level))
 
 (set-frame-parameter nil 'alpha-background 50)
 
@@ -179,3 +179,5 @@
 
 (dolist (hook '(text-mode-hook))
   (add-hook hook (lambda () (flyspell-mode 1))))
+
+(use-package nerd-icons)
--- custom.el
+++ custom.el
@@ -8,13 +8,13 @@
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files '("~/OneDrive/org/school.org"))
  '(package-selected-packages
-   '(treemacs writeroom-mode treemacs-magit treemacs-evil treemacs-all-the-icons org-xlatex org-super-agenda org-modern org-bullets olivetti fountain-mode ein)))
+   '(nerd-icons org-drill treemacs writeroom-mode treemacs-magit treemacs-evil treemacs-all-the-icons org-xlatex org-super-agenda org-modern org-bullets olivetti fountain-mode ein)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#2E3440" :foreground "#ECEFF4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "outline" :family "Consolas")))))
 (put 'customize-face 'disabled nil)
 (put 'customize-themes 'disabled nil)
 (put 'customize-variable 'disabled nil)
