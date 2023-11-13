debfd4776da486c78b1ad38beea233677e1f9ed0
Aseem Ratha <aseemratha@gmail.com>
2023-11-12 23:49:53 -0500

    idk what this time

--- custom.el
+++ custom.el
@@ -3,12 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("3cdd0a96236a9db4e903c01cb45c0c111eb1492313a65790adb894f9f1a33b2d" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files '("~/OneDrive/org/school.org"))
  '(package-selected-packages
-   '(nerd-icons org-drill treemacs writeroom-mode treemacs-magit treemacs-evil treemacs-all-the-icons org-xlatex org-super-agenda org-modern org-bullets olivetti fountain-mode ein)))
+   '(treemacs uimage treemacs-evil spell-fu smooth-scrolling smooth-scroll org-super-agenda org-modern org-fragtog org-drill olivetti nerd-icons magit latex-preview-pane fountain-mode ein dracula-theme auctex all-the-icons @)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
