54fbfadadf7c69a3341ab74103a24262e1f02027
Aseem Ratha <aseemratha@gmail.com>
2024-05-17 13:07:35 +0000

    dasdasd

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("525e440b193423c4c716521daff71ba2ed173cce3026b0a3e2424a7d4dd0b50e" "c43648edfb1b0386976f49f24dc81eb36484d078b590c93cd45aa540c83ba10f" "952747e7835aa14d9a1b8e6282be65e33ab10810bc4d116d775c8e8df5e44f32" "484d371c5e7b77aac0acc9796c12da61a255929b370d7e64cac826910ece0f27" "ac5755c85fedaf9be82b23cf93770314daf7830eb172e2b39880558114c817d8" "be5a9505c604e447fcb16a78e1d9fc21f0816bfcd04e18d88e65b3282bd98ca0" "795560d4f6ef3ed83aa905db8d63374a5b4062ab014ae9ef747e386117f9e378" "1e1bab3e91b37f596ec900df85489a35db7ba82a628bd631ace305f829bb909b" "0a81a47b5f1247dd5ceb991548a1de162031c0b4af3e7ac7d8278033e0023d92" "acb531e1de0851571a5e4c2208de1a210ec44008d2dd9d0382cf9ffdc51d2249" "42309fea7e261fe8ebdc98c698fec434cc88300d84e9eab5c718eb538a2ffa98" "c2de5d8cd7d17a64e57b8b29e569294f9e62724fcd8b89668aa5bb3570eb83a3" "59daaac9a94a02707d2d76219087b5448a0b823ed5caf9f80c33677c5c6ab5e7" "941bd1fb34ab7341d790546070c48cacba14601f99c34979fdcecc8128c033ed" "4180170269695f90bfcbd9f28478afabf0abbad30f06b9b941155795192ad141" "7bb811fd813b2a73118050ad4b96befc84b97ec04b451f7b102f11903b15422b" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "9ee523d2acd94411e7044a1c4e21f8bfae862d8bf34a19cce3a4ed8d77a5ce64" default))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(border ((t (:width expanded)))))
